68e9009281725a3061f4932304a5b5adc8ae35d4
Darksideofthemoon1 <tiernanx0101@gmail.com>
2024-03-04 15:56:15 +0000

    fixed bug regarding color not changing when changing size. Final cleanup including setting the initial color to black

--- script.js
+++ script.js
@@ -2,40 +2,20 @@ const containerDiv = document.querySelector(".container");
 const btnChangeSize = document.querySelector(".change-size");
 const btnRainbow = document.querySelector(".change-color");
 let gridSize = 16;
-const createGrid = (num) => {
-  containerDiv.innerHTML = "";
-  for (let i = 0; i < num * num; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    containerDiv.appendChild(square);
-    square.style.flex = `0 0 calc(100% / ${num})`;
-    square.style.height = `calc(500px / ${num})`;
-  }
-};
-createGrid(gridSize);
-
-const squares = document.querySelectorAll(".square");
-squares.forEach((s) => {
-  s.addEventListener("mouseover", () => {
-    s.classList.add("color");
-  });
-});
 
 btnChangeSize.addEventListener("click", () => {
-  let size = prompt("Enter a size: ");
-  if (size > 1 && size <= 100) {
-    createGrid(size);
-  } else {
-    size = "";
+  let size = parseInt(prompt("Enter a size: "));
+  if (size > 100) {
     alert("Enter a size Maximum 100: ");
+    return;
   }
+  containerDiv.textContent = "";
+  createGrid(size);
 });
 
-const rainbowMode = () => {
+const changeColor = () => {
+  const squares = document.querySelectorAll(".square");
   squares.forEach((s) => {
-    if (s.classList.contains("color")) {
-      s.classList.remove("color");
-    }
     s.addEventListener("mouseover", () => {
       let randomColor = Math.floor(Math.random() * 16777215).toString(16);
       s.style.backgroundColor = "#" + randomColor;
@@ -43,4 +23,14 @@ const rainbowMode = () => {
   });
 };
 
-btnRainbow.addEventListener("click", rainbowMode);
+const createGrid = (num) => {
+  for (let i = 0; i < num * num; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `calc(100% / ${num})`;
+    square.style.height = `calc(500px / ${num})`;
+    containerDiv.appendChild(square);
+    btnRainbow.addEventListener("click", changeColor);
+  }
+};
+createGrid(gridSize);
