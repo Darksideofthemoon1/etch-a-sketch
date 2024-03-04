8d8354a683975f3ec5061ce730214043d1e65988
Darksideofthemoon1 <tiernanx0101@gmail.com>
2024-03-04 16:10:35 +0000

    added functionality for reset button. cleaned up code project complete.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const containerDiv = document.querySelector(".container");
 const btnChangeSize = document.querySelector(".change-size");
 const btnRainbow = document.querySelector(".change-color");
 let gridSize = 16;
+const btnReset = document.querySelector(".reset");
 
 btnChangeSize.addEventListener("click", () => {
   let size = parseInt(prompt("Enter a size: "));
@@ -13,12 +14,29 @@ btnChangeSize.addEventListener("click", () => {
   createGrid(size);
 });
 
+const resetGrid = () => {
+  containerDiv.textContent = "";
+  createGrid(gridSize);
+};
+
 const changeColor = () => {
   const squares = document.querySelectorAll(".square");
   squares.forEach((s) => {
+    if (s.classList.contains("black")) {
+      s.classList.remove("black");
+    }
     s.addEventListener("mouseover", () => {
       let randomColor = Math.floor(Math.random() * 16777215).toString(16);
       s.style.backgroundColor = "#" + randomColor;
+    });
+  });
+};
+
+const setBlack = () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((s) => {
+    s.addEventListener("mouseover", () => {
+      s.classList.add("black");
     });
   });
 };
@@ -30,6 +48,8 @@ const createGrid = (num) => {
     square.style.width = `calc(100% / ${num})`;
     square.style.height = `calc(500px / ${num})`;
     containerDiv.appendChild(square);
+    setBlack();
+    btnReset.addEventListener("click", resetGrid);
     btnRainbow.addEventListener("click", changeColor);
   }
 };
